eb8c0d1de6a7c45d6f0886ea84234c6ddb366465
Hamad Alafeefi <60110135+HAlafeefi@users.noreply.github.com>
2026-03-02 12:59:29 +0400

    clipboard with glippy

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
 	"strings"
 
 	"github.com/alafeefidev/mdme"
+	"github.com/f1bonacc1/glippy"
 )
-//TODO add copy to keyboard
+
+// TODO add copy to keyboard
 func main() {
+	var verbose bool
+	var suppress bool
+
+	flag.BoolVar(&verbose, "verbose", false, "enable debug logging")
+	flag.BoolVar(&verbose, "v", false, "enable debug logging (shorthand)")
+	flag.BoolVar(&suppress, "suppress", false, "suppress console output, only copy to clipboard")
+	flag.BoolVar(&suppress, "s", false, "suppress console output, only copy to clipboard (shorthand)")
 
 	// Current directory and no path provided, immediately a flag
 	path := "."
@@ -32,6 +42,16 @@ func main() {
 		}
 	}
 
+	logLevel := slog.LevelInfo
+	if verbose {
+		logLevel = slog.LevelDebug
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+	slog.SetDefault(logger)
+
 	files, err := mdme.ListFiles(path)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error parsing files: %v\n", mdme.ErrorMsg(err))
@@ -44,5 +64,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Print(md)
+	// Raise error only if output is supressed, else ignore
+	err = glippy.Set(md)
+	if !suppress {
+		fmt.Print(md)
+	} else if suppress && err != nil {
+		fmt.Fprintf(os.Stderr, "Error pasting output to clipboard: %v\n", mdme.ErrorMsg(err))
+		os.Exit(1)
+	}
+	
+	
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/alafeefidev/mdme
 
 go 1.25.0
+
+require github.com/f1bonacc1/glippy v1.1.0
+
+require github.com/jezek/xgb v1.1.0 // indirect
